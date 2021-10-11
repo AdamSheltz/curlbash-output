bc00daac64c39181f9c12057c46094cc8633d4cd
AdamSheltz <77626929+AdamSheltz@users.noreply.github.com>
2021-10-11 18:58:14 -0400

    Update stderrout.bash

--- stderrout.bash
+++ stderrout.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 rdate=$(date "+%Y%m%d.%H%M")
-echo "VAMPIRE" >> /var/log/messages
-echo $rdate  2>&1 >> /var/log/messages
-echo "VAMPIRE" >> /var/log/messages
+echo "VAMPIRE" >> logga
+echo $rdate  2>&1 >> logga
+echo "VAMPIRE" >> logga
